6ad414ebbd2e7a5f534704876ab834c9507a2568
nitin1229 <nitin.gupta@geminisolutions.com>
2025-09-17 17:29:08 +0530

    api service and env configurations added

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -1,15 +1,34 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'environments/environment';
 import { ENVIRONMENTS } from '../constants/constants';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
+import {
+  DEV_CONFIG,
+  PREPROD_CONFIG,
+  PROD_CONFIG,
+  UAT_CONFIG,
+} from '../configurations/configuration';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
-  environment = '';
+  environment: string = '';
+  public url: string = '';
+  public loginUrl: string;
+  public paymentUrl: string = '';
+  public domain: string = '';
+  public product_name = 'rs-prjkt';
+  public domainUrl: string = '';
+  public productId: string = '';
 
-  constructor() {
-    console.log('test');
+  commonPath: string = '';
+  commonCssPath: string = '';
+  commonJsPath: string = '';
+
+  constructor(private readonly http: HttpClient) {
+    this.loginUrl = '/ipdsv2/login/#/';
     const { host } = window.location;
 
     if (host == 'localhost:4202') {
@@ -25,13 +44,124 @@ export class ApiService {
     }
 
     if (this.environment == ENVIRONMENTS.DEV) {
-      console.log('Dev Environment');
+      const {
+        url,
+        domain,
+        domainUrl,
+        productId,
+        commonPath,
+        commonCssPath,
+        commonJsPath,
+      } = DEV_CONFIG;
+
+      Object.assign(this, {
+        url,
+        domain,
+        domainUrl,
+        productId,
+        commonPath,
+        commonCssPath,
+        commonJsPath,
+      });
     } else if (this.environment == ENVIRONMENTS.UAT) {
-      console.log('UAT Environment');
+      const {
+        url,
+        domain,
+        domainUrl,
+        productId,
+        commonPath,
+        commonCssPath,
+        commonJsPath,
+      } = UAT_CONFIG;
+
+      Object.assign(this, {
+        url,
+        domain,
+        domainUrl,
+        productId,
+        commonPath,
+        commonCssPath,
+        commonJsPath,
+      });
     } else if (this.environment == ENVIRONMENTS.PREPROD) {
-      console.log('Preprod Environment');
+      const {
+        url,
+        domain,
+        domainUrl,
+        productId,
+        commonPath,
+        commonCssPath,
+        commonJsPath,
+      } = PREPROD_CONFIG;
+
+      Object.assign(this, {
+        url,
+        domain,
+        domainUrl,
+        productId,
+        commonPath,
+        commonCssPath,
+        commonJsPath,
+      });
     } else if (this.environment == ENVIRONMENTS.PROD) {
-      console.log('Prod Environment');
+      const {
+        url,
+        domain,
+        domainUrl,
+        productId,
+        commonPath,
+        commonCssPath,
+        commonJsPath,
+      } = PROD_CONFIG;
+
+      Object.assign(this, {
+        url,
+        domain,
+        domainUrl,
+        productId,
+        commonPath,
+        commonCssPath,
+        commonJsPath,
+      });
+    }
+  }
+
+  /****  Common HTTP POST Method****/
+  async httpPostMethod(url: string, parameter: any): Promise<any[]> {
+    try {
+      const response: any = await firstValueFrom(
+        this.http.post(url, parameter, { observe: 'response' }),
+      );
+      const data = response['body'];
+      return data;
+    } catch (err: any) {
+      throw new Error(err?.error || 'Server error');
+    }
+  }
+
+  /****  Common HTTP Get Method****/
+  async httpGetMethod(url: string): Promise<any[]> {
+    try {
+      const response: any = await firstValueFrom(
+        this.http.post(url, { observe: 'response' }),
+      );
+      const data = response['body'];
+      return data;
+    } catch (err: any) {
+      throw new Error(err?.error || 'Server error');
+    }
+  }
+
+  /****  Common HTTP Delete Method****/
+  async httpDeleteMethod(url: string): Promise<any[]> {
+    try {
+      const response: any = await firstValueFrom(
+        this.http.delete(url, { observe: 'response' }),
+      );
+      const data = response['body'];
+      return data;
+    } catch (err: any) {
+      throw new Error(err?.error || 'Server error');
     }
   }
 }
